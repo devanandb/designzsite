2f58f21a897c798432b7b055e5a8b7d7dd5c6ee1
Devanand <dev.ryder88@gmail.com>
2014-05-24 21:27:37 +0530

    fixing db issues

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -47,10 +47,10 @@ return array(
 	'connections' => array(
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'designz',
-			'username'  => 'root',
-			'password'  => '',
+			'host'      => getenv('DB_HOST'),
+			'database'  => getenv('DB_NAME'),
+			'username'  => getenv('DB_USERNAME'),
+			'password'  => getenv('DB_PASSWORD'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
